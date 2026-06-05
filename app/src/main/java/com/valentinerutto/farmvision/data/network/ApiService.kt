859766e2b31ae9aa2a58450d3fc3d6557be6ea6b
valentinerutto <valentinrutto@gmail.com>
2026-06-05 17:37:34 +0300

    Update ApiService with parameterized weather query and multipart tree analysis

--- app/src/main/java/com/valentinerutto/farmvision/data/network/ApiService.kt
+++ app/src/main/java/com/valentinerutto/farmvision/data/network/ApiService.kt
@@ -1,15 +1,30 @@
 package com.valentinerutto.farmvision.data.network
 
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 import retrofit2.http.GET
+import retrofit2.http.Multipart
 import retrofit2.http.POST
+import retrofit2.http.Part
+import retrofit2.http.Query
 
 interface ApiService {
 
-    @GET("weather-geo")
-    suspend fun getWeatherForecast(): WeatherResponse
-
+    @GET("weather")
+    suspend fun getWeather( @Query("lat") lat: Double,
+                                        @Query("lon") lon: Double,
+                                        @Query("days") days: Int = 5,
+                                        @Query("ai") ai: Boolean = true,
+                                        @Query("units") units: String = "metric",
+                                        @Query("lang") lang: String = "en"): WeatherResponse
+@Multipart
     @POST("trees/analyze")
-    suspend fun getAnalyzedImage() : TreeAnalysisResponse
+    suspend fun analyzeTrees(  @Part image: MultipartBody.Part,
+                                   @Part("farmerId") farmerId: RequestBody?,
+                                   @Part("county") county: RequestBody?,
+                                   @Part("landAcres") landAcres: RequestBody?,
+                                   @Part("location") location: RequestBody?,
+                                   @Part("notes") notes: RequestBody?) : TreeAnalysisResponse
 
 
 
